8392de7987026ff34bc987e09d0e6281a44fa629
Job <0326531@student.rocvantwente.nl>
2022-11-14 10:17:04 +0100

    data compiler changes

--- DataCompiler/StarterDeck_DataCompiler/StarterDeck_DataCompiler/Forms/TechniquesForm.Designer.cs
+++ DataCompiler/StarterDeck_DataCompiler/StarterDeck_DataCompiler/Forms/TechniquesForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.TechniquesGridView = new System.Windows.Forms.DataGridView();
+            this.SubmitButton = new System.Windows.Forms.Button();
             this.Category = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PhaseSelection1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
@@ -37,7 +38,6 @@
             this.PhaseSelection3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.PhaseSelection4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.Cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SubmitButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TechniquesGridView)).BeginInit();
             this.SuspendLayout();
@@ -79,6 +79,19 @@
             this.TechniquesGridView.Size = new System.Drawing.Size(1226, 390);
             this.TechniquesGridView.TabIndex = 0;
             // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SubmitButton.Location = new System.Drawing.Point(3, 399);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(1226, 24);
+            this.SubmitButton.TabIndex = 1;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
             // Category
             // 
             this.Category.HeaderText = "Category";
@@ -117,26 +130,12 @@
             this.Cost.HeaderText = "Cost";
             this.Cost.Name = "Cost";
             // 
-            // SubmitButton
-            // 
-            this.SubmitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.SubmitButton.Location = new System.Drawing.Point(3, 399);
-            this.SubmitButton.Name = "SubmitButton";
-            this.SubmitButton.Size = new System.Drawing.Size(1226, 24);
-            this.SubmitButton.TabIndex = 1;
-            this.SubmitButton.Text = "Submit";
-            this.SubmitButton.UseVisualStyleBackColor = true;
-            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
-            // 
             // TechniquesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1256, 450);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "TechniquesForm";
+            this.Controls.Add(this.tableLayoutPanel1);          
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Techniques";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TechniquesForm_FormClosing);
@@ -150,6 +149,7 @@
 
         private TableLayoutPanel tableLayoutPanel1;
         private DataGridView TechniquesGridView;
+        private Button SubmitButton;
         private DataGridViewComboBoxColumn Category;
         private DataGridViewTextBoxColumn Name;
         private DataGridViewCheckBoxColumn PhaseSelection1;
@@ -157,6 +157,5 @@
         private DataGridViewCheckBoxColumn PhaseSelection3;
         private DataGridViewCheckBoxColumn PhaseSelection4;
         private DataGridViewTextBoxColumn Cost;
-        private Button SubmitButton;
     }
 }
